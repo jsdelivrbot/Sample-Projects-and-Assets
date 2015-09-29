87c59e75576ed515042d4bc24b80e6b2690d59c1
Clint Fleetwood <clint.fleetwood@gmail.com>
2015-09-29 15:11:36 -0400

    made a few more changes to the sample

--- game_setup_samples/adventure-sample/startScripts.js
+++ game_setup_samples/adventure-sample/startScripts.js
@@ -5,13 +5,13 @@ var myGame = {
       file : 'chris.babylon',
       importMeshName:'Chris',
       boundsSize : {
-        x : 0.1,
-        y: 0.3,
-        z : 0.1
+        x : 0.2,
+        y: 0.5,
+        z : 0.2
       },
       boundsOffset : {
         x : 0,
-        y: 0.5,
+        y: 0.9,
         z: 0
       },
       scale : {
