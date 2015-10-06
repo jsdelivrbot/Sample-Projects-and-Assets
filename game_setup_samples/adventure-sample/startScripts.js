b715682f2e6b3d7595343546301ae06e9451f267
Clint Fleetwood <clint.fleetwood@gmail.com>
2015-10-06 15:22:04 -0400

    updated the example to be a little bit better

--- game_setup_samples/adventure-sample/startScripts.js
+++ game_setup_samples/adventure-sample/startScripts.js
@@ -1,5 +1,32 @@
-var myGame = {
-  config : {
+A3D.config = {
+  game:{
+    forceResolution: true,
+    targetRes : [640,480],
+    startScene : 'sampleLevel'
+  },
+  scenes : {
+    sampleLevel : {
+      rootUrl : './scenes/',
+      file : 'scene1.babylon',
+      onload : function(){
+        var pat = {
+            speed:0.03,
+            patrolPoints:[
+              {speed:0.1,gotoPoint:{x:0,y:0,z:0},lookTo:true},
+              {gotoPoint:{x:3,y:0,z:3},lookTo:{x:0,y:0,z:0}},
+              {gotoPoint:{x:8,y:0,z:-5},lookTo:true},
+              {gotoPoint:{x:-7,y:0,z:3},wait:2000,lookTo:true,speed:0.01}
+            ]
+          };
+        console.log('this is nice');
+        for(var e in A3D.ActiveGame.mainScene.Enemy){
+          var me = A3D.ActiveGame.mainScene.Enemy[e];
+          me.makePatrol(pat);
+          me.startPatrol();
+        }
+      }
+    }
+  },
     playerSetup : {
       urlRoot : './models/',
       file : 'chris.babylon',
@@ -84,20 +111,18 @@ var myGame = {
         z:0
       }
     }
-  },
-
-  scripts : function(s){
-    A3D.ActiveGame.run();
-  }
-};
+  };
 
 
 function start(){
-    new A3D.Game(myGame.config);
+  new A3D.Game();
+  A3D.ActiveGame.run();
+    /*
     A3D.ActiveGame._loadScene('./scenes/','sampleLevel.babylon',function(){
       myGame.Adventure = new A3D.Module.Adventure(myGame.config);
       myGame.Adventure.loadModule(myGame.scripts);
     });
+    */
 }
 
 document.addEventListener( "DOMContentLoaded", start, false );
